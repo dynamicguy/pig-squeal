831dcce24840831b062c21bb0998544cd8f73c66
James Lampton <jlampton@gmail.com>
2013-03-02 14:12:20 -0500

    Fixed execution to appropriately connect to hdfs when necessary.
    Added missing JoinFixer.  Should probably manage the temp files better.

--- src/org/apache/pig/impl/storm/SExecutionEngine.java
+++ src/org/apache/pig/impl/storm/SExecutionEngine.java
@@ -1,5 +1,7 @@
 package org.apache.pig.impl.storm;
 
+import org.apache.pig.ExecType;
+import org.apache.pig.backend.executionengine.ExecException;
 import org.apache.pig.backend.hadoop.executionengine.HExecutionEngine;
 import org.apache.pig.impl.PigContext;
 
@@ -10,4 +12,18 @@ public class SExecutionEngine extends HExecutionEngine {
 		// TODO Auto-generated constructor stub
 	}
 
+	public void init() throws ExecException {
+		// Pull the pig ExecType and change it to fool HExecutionEngine.
+		ExecType memo = pigContext.getExecType();
+		if (pigContext.getExecType() == ExecType.STORM) {
+			pigContext.setExecType(ExecType.MAPREDUCE);
+		} else if (pigContext.getExecType() == ExecType.STORMLOCAL) {
+			pigContext.setExecType(ExecType.LOCAL);
+		}
+        super.init();
+        // Restore it if necessary.
+        if (pigContext.getExecType() != memo) {
+        	pigContext.setExecType(memo);
+        }
+    }
 }
